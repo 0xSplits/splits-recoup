1b8d05b85363623c04fe572c271bf46b153b0f79
Mike H <mikeh09@gmail.com>
2023-02-15 14:53:55 -0800

    run linter

--- src/Recoup.sol
+++ src/Recoup.sol
@@ -70,76 +70,76 @@ contract Recoup {
     /// (last recipient has no threshold & receives all residual flows)
     /// @dev A single address in a recipient array with a matching single 1e6 value in a percentAllocations array means that tranche will be a single address and not a split
     function createRecoup(
-      address token,
-      address nonWaterfallRecipient, // Worth having this? Don't think we expose it in our ui currently, so can't see us adding it for this (but maybe good to have as an option for future ui updates)
-      uint32 distributorFee, // Worth even having this? Should it always be 0? If the waterfall has no distribution incentive, seems like minimal gain to have it on the splits
-      address[][] calldata recipients,
-      uint32[][] calldata percentAllocations,
-      uint256[] calldata thresholds
+        address token,
+        address nonWaterfallRecipient, // Worth having this? Don't think we expose it in our ui currently, so can't see us adding it for this (but maybe good to have as an option for future ui updates)
+        uint32 distributorFee, // Worth even having this? Should it always be 0? If the waterfall has no distribution incentive, seems like minimal gain to have it on the splits
+        address[][] calldata recipients,
+        uint32[][] calldata percentAllocations,
+        uint256[] calldata thresholds
     ) external {
-      /// checks
+        /// checks
 
-      uint256 recipientsLength = recipients.length;
+        uint256 recipientsLength = recipients.length;
 
-      // ensure recipients array and percent allocations array match in length
-      if (recipientsLength != percentAllocations.length) {
-        revert InvalidRecoup__RecipientsAndPercentAllocationsMismatch();
-      }
-
-      uint256 i = 0;
-      for (; i < recipientsLength;) {
-        // TODO: better way to setup these checks?
-
-        uint256 recipientsIndexLength = recipients[i].length;
-        if (recipientsIndexLength != percentAllocations[i].length) {
-          revert InvalidRecoup__AccountsAndPercentAllocationsMismatch(i);
-        }
-        if (recipientsIndexLength == 0) {
-          revert InvalidRecoup__TooFewAccounts(i);
-        }
-        if (recipientsIndexLength == 1 && percentAllocations[i][0] != PERCENTAGE_SCALE) {
-          revert InvalidRecoup__SingleAddressPercentAllocation(i, percentAllocations[i][0]);
-        }
-        // Other recipient/percent allocation combos are splits and will get validated in the create split call
-
-        unchecked {
-          ++i;
-        }
-      }
-
-      /// effects
-      address[] memory waterfallRecipients = new address[](recipientsLength);
-      
-      // Create splits
-      i = 0;
-      for (; i < recipientsLength;) {
-        if (recipients[i].length == 1) {
-          waterfallRecipients[i] = recipients[i][0];
-        } else {
-          // Will fail if it's an immutable split that already exists. The caller
-          // should just pass in the split address (with percent = 100%) in that case
-          waterfallRecipients[i] = splitMain.createSplit({
-              accounts: recipients[i],
-              percentAllocations: percentAllocations[i],
-              distributorFee: distributorFee,
-              controller: address(0)
-          });
+        // ensure recipients array and percent allocations array match in length
+        if (recipientsLength != percentAllocations.length) {
+            revert InvalidRecoup__RecipientsAndPercentAllocationsMismatch();
         }
 
-        unchecked {
-          ++i;
+        uint256 i = 0;
+        for (; i < recipientsLength;) {
+            // TODO: better way to setup these checks?
+
+            uint256 recipientsIndexLength = recipients[i].length;
+            if (recipientsIndexLength != percentAllocations[i].length) {
+                revert InvalidRecoup__AccountsAndPercentAllocationsMismatch(i);
+            }
+            if (recipientsIndexLength == 0) {
+                revert InvalidRecoup__TooFewAccounts(i);
+            }
+            if (recipientsIndexLength == 1 && percentAllocations[i][0] != PERCENTAGE_SCALE) {
+                revert InvalidRecoup__SingleAddressPercentAllocation(i, percentAllocations[i][0]);
+            }
+            // Other recipient/percent allocation combos are splits and will get validated in the create split call
+
+            unchecked {
+                ++i;
+            }
         }
-      }
 
-      // Create waterfall
-      IWaterfallModule wm = waterfallModuleFactory.createWaterfallModule({
-          token: token,
-          nonWaterfallRecipient: nonWaterfallRecipient,
-          recipients: waterfallRecipients,
-          thresholds: thresholds
-      });
+        /// effects
+        address[] memory waterfallRecipients = new address[](recipientsLength);
 
-      // TODO: do i need the created split addresses here too? Technically subgraph can look those up from the waterfall I think.
-      emit CreateRecoup(address(wm));
+        // Create splits
+        i = 0;
+        for (; i < recipientsLength;) {
+            if (recipients[i].length == 1) {
+                waterfallRecipients[i] = recipients[i][0];
+            } else {
+                // Will fail if it's an immutable split that already exists. The caller
+                // should just pass in the split address (with percent = 100%) in that case
+                waterfallRecipients[i] = splitMain.createSplit({
+                    accounts: recipients[i],
+                    percentAllocations: percentAllocations[i],
+                    distributorFee: distributorFee,
+                    controller: address(0)
+                });
+            }
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        // Create waterfall
+        IWaterfallModule wm = waterfallModuleFactory.createWaterfallModule({
+            token: token,
+            nonWaterfallRecipient: nonWaterfallRecipient,
+            recipients: waterfallRecipients,
+            thresholds: thresholds
+        });
+
+        // TODO: do i need the created split addresses here too? Technically subgraph can look those up from the waterfall I think.
+        emit CreateRecoup(address(wm));
     }
 }
