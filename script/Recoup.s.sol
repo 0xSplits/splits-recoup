448deb2556c0e76364c14d7b7ead6de67b0fcb9f
Mike H <mikeh09@gmail.com>
2023-03-09 14:26:08 -0800

    revert bsc changes

--- script/Recoup.s.sol
+++ script/Recoup.s.sol
@@ -9,8 +9,7 @@ contract RecoupScript is Script {
         uint256 privKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(privKey);
 
-        // new Recoup{salt: keccak256("0xSplits.recoup.v1")}(0x2ed6c4B5dA6378c7897AC67Ba9e43102Feb694EE, 0x4Df01754eBd055498C8087b1e9a5c7a9ad19b0F6);
-        new Recoup{salt: keccak256("0xSplits.bsc.recoup.v1")}(0x5924cD81dC672151527B1E4b5Ef57B69cBD07Eda, 0xB7CCCcCeb459F0910589556123dC5fA6DC8dE4E0);
+        new Recoup{salt: keccak256("0xSplits.recoup.v1")}(0x2ed6c4B5dA6378c7897AC67Ba9e43102Feb694EE, 0x4Df01754eBd055498C8087b1e9a5c7a9ad19b0F6);
 
         vm.stopBroadcast();
     }
